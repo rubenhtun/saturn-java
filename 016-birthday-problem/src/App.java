a2bbf0d5a92866cc4850296bbe5813eccaa0bfa6
Ruben Htun <rubenhtun2003@gmail.com>
2025-06-10 16:46:06 +0700

    birthday problem

--- 016-birthday-problem/src/App.java
+++ 016-birthday-problem/src/App.java
@@ -2,22 +2,22 @@ public class App {
     public static void main(String[] args) {
         // Number of days per year
         boolean[] used = new boolean[365];
-        // Count times to find the same number;
+        // Count the number of tries to find a repeated birthday
         int count = 0;
 
-        // Harnessing While Loop for endless iteration
+        // Harnessing While Loop for iteration until a repeated birthday is found
         while (true) {
-            // Get a random birtday date
-            int birtday = (int)(Math.random()*365);
+            // Get a random birthday date
+            int birthday = (int)(Math.random()*365);
             count++; // Increment of counting times
 
-            // Check if the same birthday has been found for another person
-            if(used[birtday]) {
+            // Check if the same birthday has already been recorded
+            if(used[birthday]) {
                 break; // Stop the iteration
             }
 
-            // If not, to make sure to record that birthday has already found
-            used[birtday] = true;
+            // If not, record that the birthday has been found
+            used[birthday] = true;
         }
 
         // Show the result
